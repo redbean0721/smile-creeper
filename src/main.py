df0266f72c4cbf1bb93f49560fe76197e552fc48
redbean0721 <redbean0721@gmail.com>
2023-07-16 09:48:58 +0800

    Add version number

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import discord
-import os # default module
+import os, json # default module
 from dotenv import load_dotenv
+
+with open('version.json', mode='r',encoding='utf8') as v:
+    version = json.load(v)
 
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} is ready and online!")
+    print(f"{bot.user} is ready and online!\nCurrent Version: ", version['version'])
 
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
