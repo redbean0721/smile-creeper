e019d8e5210fbd3d6c3447dc47640e0ae42a8f26
redbean0721 <redbean0721@gmail.com>
2023-07-19 18:19:54 +0800

    RepairError - Can't remove role

--- src/cmds/reaction.py
+++ src/cmds/reaction.py
@@ -36,29 +36,26 @@ class Reaction(Cog_Extension):
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload:discord.RawReactionActionEvent):
         guild = self.bot.get_guild(payload.guild_id)
+        user = guild.get_member(payload.user_id)
         # 判斷反應貼圖移除相對應身分組
         if payload.message_id == 1129973522026995763:
             if str(payload.emoji) == "✅":
                 role = guild.get_role(1129975095314620617)
-                user = guild.get_member(payload.user_id)
                 await user.remove_roles(role)
                 # await user.send(f'你移除了 {role} 身分組')
                 print(f'Remove {role} to {payload.member}, {payload.emoji} in {guild}')
             if str(payload.emoji) == "❤️":
                 role = guild.get_role(1129766237988192296)
-                user = guild.get_member(payload.user_id)
                 await user.remove_roles(role)
                 # await user.send(f'你移除了 {role} 身分組')
                 print(f'Remove {role} to {payload.member}, {payload.emoji} in {guild}')
             if str(payload.emoji) == "♂️":
                 role = guild.get_role(1129351320864837733)
-                user = guild.get_member(payload.user_id)
                 await user.remove_roles(role)
                 # await user.send(f'你移除了 {role} 身分組')
                 print(f'Remove {role} to {payload.member}, {payload.emoji} in {guild}')
             if str(payload.emoji) == "♀️":
                 role = guild.get_role(1129351260550725662)
-                user = guild.get_member(payload.user_id)
                 await user.remove_roles(role)
                 # await user.send(f'你移除了 {role} 身分組')
                 print(f'Remove {role} to {payload.member}, {payload.emoji} in {guild}')
